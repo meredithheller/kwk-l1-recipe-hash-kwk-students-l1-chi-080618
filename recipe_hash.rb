4fbbe7c9223e78b6ad5a12a82f5308cbc9f0d45c
meredithheller <meredithheller@outlook.com>
2018-08-09 15:20:18 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -30,6 +30,7 @@ def recipe_ingredients(perfect_10_recipe)
   return
 end
 
+recipe_ingredients
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(perfect_10_recipe)
   return perfect_10_recipe
